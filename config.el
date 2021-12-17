999e83de091a51401db7019ea34fc52309919cf6
João Dowsley <joao.dowsley850@gmail.com>
2021-12-16 22:44:52 -0300

    Projects folder

--- config.el
+++ config.el
@@ -36,7 +36,10 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/desktop/Org/")
+(setq
+ org-directory "~/desktop/Org/"
+ projectile-project-search-path '("~/desktop/Code/")
+)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
